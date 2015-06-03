b7d8aac5de153a406c44c4c6419d2300d9f596b7
LucasIsasmendi <tartalario@yahoo.com.ar>
2015-06-03 11:22:24 -0300

    Loading popup

--- www/app/services/eliteApi.js
+++ www/app/services/eliteApi.js
@@ -1,7 +1,7 @@
 (function() {
 	'use strict';
-	angular.module('eliteApp').factory('eliteApi', ['$http', '$q', eliteApi]);
-	function eliteApi($http, $q) {
+	angular.module('eliteApp').factory('eliteApi', ['$http', '$q', '$ionicLoading', eliteApi]);
+	function eliteApi($http, $q, $ionicLoading) {
     var currentLeagueId;
 
 		function getLeagues(){
@@ -17,14 +17,17 @@
       return deferred.promise        
 		}
 		function getLeagueData(){
-      var deferred = $q.defer()
+      var deferred = $q.defer();
+      $ionicLoading.show({ template: 'Loading...'});
       $http.get("http://elite-schedule.net/api/leaguedata/" + currentLeagueId)
         .success(function(data, status){
           console.log("Received schedule data via HTTP.", data, status);
+          $ionicLoading.hide();
           deferred.resolve(data);
         })
         .error(function(){
           console.log("Error while making HTTP call.");
+          $ionicLoading.hide();          
           deferred.reject();
         });
       return deferred.promise
