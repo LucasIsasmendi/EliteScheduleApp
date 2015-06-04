de1de001820f05ec1a8dde5fe22732b9edddc16c
LucasIsasmendi <tartalario@yahoo.com.ar>
2015-06-04 13:25:52 -0300

    showdown marker rules directive

--- www/app/locations/location-map-ctrl.js
+++ www/app/locations/location-map-ctrl.js
@@ -1,8 +1,8 @@
 (function() {
 	'use strict';
-	angular.module('eliteApp').controller('LocationMapCtrl', ['$stateParams', LocationMapCtrl]);
+	angular.module('eliteApp').controller('LocationMapCtrl', ['$stateParams', 'eliteApi', LocationMapCtrl]);
 	
-	function LocationMapCtrl($stateParams) {
+	function LocationMapCtrl($stateParams, eliteApi) {
 		var vm = this;
 
 		vm.locationId = Number($stateParams.id);
@@ -12,8 +12,33 @@
 				latitude: 38.897677,
 				longitude: -77.036530
 			},
-			zoom: 12
+			zoom: 15
 		};
 		vm.marker = {}
+
+		eliteApi.getLeagueData().then(function(data) {
+			vm.location = _.find(data.locations, { id: vm.locationId });
+			vm.marker = {
+				id:0,
+				coords: {
+			        latitude: vm.location.latitud,
+			        longitude: vm.location.longitude
+				}
+			};
+         vm.marker.options = {
+            draggable: true,
+            labelContent: "lat: " + vm.location.longitude + ' ' + 'lon: ' + vm.location.latitud,
+            labelAnchor: "100 0",
+            labelClass: "marker-labels",
+		        title: vm.location.name + "<br/>(Tap for directions)",
+            	showWindow: true            
+          };			
+			vm.map.center.latitude = vm.location.latitude;
+			vm.map.center.longitude = vm.location.longitude;
+		});
+		vm.locationClicked = function(marker){
+			console.log("locationClicked", marker);
+			window.location = "geo: " + marker.coords.latitude +","+ marker.coords.longitude;
+		}
 	};
 })();
